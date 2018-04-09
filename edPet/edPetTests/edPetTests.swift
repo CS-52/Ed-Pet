3d0bb67449fcd7b90f8f191c8e0ddf9697c05280
sweyns <34739872+sweyns@users.noreply.github.com>
2018-04-09 16:11:05 -0700

    Add files via upload

---
+++ edPet/edPetTests/edPetTests.swift
@@ -0,0 +1,36 @@
+//
+//  edPetTests.swift
+//  edPetTests
+//
+//  Created by Stephen Dirk M. Weyns on 9/4/18.
+//  Copyright © 2018 Stephen Dirk M. Weyns. All rights reserved.
+//
+
+import XCTest
+@testable import edPet
+
+class edPetTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
