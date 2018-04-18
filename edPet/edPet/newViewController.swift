288fdc01171bce66ba7f7814d6efbff3c71e25d2
Natalie Cygan <nataliemcygan@gmail.com>
2018-04-17 18:34:35 -0700

    Most recent version

--- edPet/edPet/newViewController.swift
+++ edPet/edPet/newViewController.swift
@@ -7,3 +7,30 @@
 //
 
 import Foundation
+import UIKit
+
+class newViewController: UIViewController {
+    
+    //IBOutlet has already been assigned a value
+    @IBOutlet weak var label: UILabel! //IB = interface builder, ! means non-optional
+    //non-optional variable can never be null or void
+    
+    //for constants, use 'let' instead of 'var'
+    
+    //automatically called when view is loaded
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        label.text = "Page forward label"
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
