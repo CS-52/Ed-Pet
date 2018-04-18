1f4be8279e7c0aa337bea4a8608ba3b31c12a2b2
Natalie Cygan <nataliemcygan@gmail.com>
2018-04-17 19:02:06 -0700

    Added folders, storyboards, and view controller files for the three main screens

--- edPet/edPet/Home Screen/homescreenViewController.swift
+++ edPet/edPet/Home Screen/homescreenViewController.swift
@@ -1,9 +1,28 @@
 //
-//  homescreenViewController.swift
+//  loginViewController.swift
 //  edPet
 //
 //  Created by Natalie Cygan on 4/17/18.
 //  Copyright © 2018 Stephen Dirk M. Weyns. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class homescreenViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func myUnwind(for unwindSegue: UIStoryboardSegue, towardsViewController subsequentVC: UIViewController) {
+        //
+    }
+    
+    
+}
--- edPet/edPet/Login/loginViewController.swift
+++ edPet/edPet/Login/loginViewController.swift
@@ -6,4 +6,25 @@
 //  Copyright © 2018 Stephen Dirk M. Weyns. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class loginViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func myUnwind(for unwindSegue: UIStoryboardSegue, towardsViewController subsequentVC: UIViewController) {
+        //
+    }
+    
+    
+}
+
+
